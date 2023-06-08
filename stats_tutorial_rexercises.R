986044f68b752f9f5ceafa0e046be7e930fdcfb9
Nick Glättli <86017714+NickGlaettli@users.noreply.github.com>
2023-06-08 09:56:17 +0200

    Update stats_tutorial_rexercises.R

--- stats_tutorial_rexercises.R
+++ stats_tutorial_rexercises.R
@@ -15,6 +15,15 @@ cov(swiss$Fertility, swiss$Agriculture)
 var(swiss$Fertility)
 sd(swiss$Fertility)
 
+#exam question:
+#Zwei Variablen lauten «Fertility» (Fertilitätsrate einer Gemeinde = Anzahl Geburten pro 1000 Frauen) 
+#und «Agriculture» (Prozentualer Anteil in der Landwirtschaft beschäftigten Männer). Uns interessiert nun,
+#wie stark der lineare Zusammenhang zwischen beiden Variablen ist.
+head(swiss)
+?cor
+cor(swiss$Fertility, swiss$Agriculture)
+
+
 #plots
 library(tidyverse)
 
@@ -29,5 +38,19 @@ ggplot(swiss, aes(x=Fertility, y=Agriculture))+
 
 #Boxplot
 ggplot(swiss, aes(x="", y=Fertility))+
+  geom_boxplot(width=0.2)+
+  scale_y_continuous(breaks = seq(40,90,10))
+
+#Boxplots: exam question
+ggplot(swiss, aes(x=factor(f_agriculture), y=Fertility))+
+  geom_boxplot(width=0.2)+
+  scale_y_continuous(breaks = seq(40,90,10))
+
+#Why factor(f_agriculture)?
+class(swiss$f_agriculture)
+is.numeric(swiss$f_agriculture)
+head(swiss)
+
+ggplot(swiss, aes(x=f_agriculture, y=Fertility))+
   geom_boxplot(width=0.2)+
   scale_y_continuous(breaks = seq(40,90,10))
